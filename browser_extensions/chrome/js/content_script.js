4b316e4ec74bf80ad465ad52db2bb67193e9cb34
Vinta <vinta.chen@gmail.com>
2019-01-10 00:57:52 +0800

    cleanup

--- browser_extensions/chrome/js/content_script.js
+++ browser_extensions/chrome/js/content_script.js
@@ -18,65 +18,6 @@ function go_node_spacing(node) {
   is_spacing = false;
 }
 
-var SAY_HELLOS = [
-  '空格之神顯靈了！',
-  '空格之神準備好了！',
-  '空格之神這不是來了嗎！',
-  '空格之神給您拜個晚年',
-  '空格之神 到此一遊',
-  '空格之神 在此！',
-  '空格之神 參見！',
-  '空格之神 參上！',
-  '空格之神 參戰！',
-  '空格之神 登場！',
-  '空格之神 來也！',
-  '空格之神 來囉！',
-  '空格之神 駕到！',
-  '空格之神 報到！',
-  '空格之神 合流！',
-  '空格之神 久違了',
-  '空格之神 作用中',
-  '空格之神 接近中',
-  '空格之神 小別勝新婚',
-  '空格之神 姍姍來遲',
-  '空格之神 完美落地',
-  '空格之神 粉墨登場！',
-  '空格之神 颯爽登場！',
-  '空格之神 強勢登場！',
-  '空格之神 強勢回歸！',
-  '空格之神 在此聽候差遣',
-  '空格之神 射出！',
-  '空格之神：寶傑好，大家好，各位觀眾朋友晚安',
-  '空格之神：歐啦歐啦歐啦歐啦歐啦',
-  '空格之神：你知不知道什麼是噹噹噹噹噹噹噹？',
-  '空格之神：悄悄的我走了，正如我悄悄的來',
-  '有請...... 空格之神！',
-  '遭遇！野生的空格之神！',
-  '就決定是你了！空格之神！',
-  '正直、善良和空格都回來了'
-];
-
-function ask_can_notify(just_notify) {
-  if (typeof alertify !== 'undefined') {
-    alertify.custom = alertify.extend('custom');
-
-    var msg = SAY_HELLOS[Math.floor(Math.random() * SAY_HELLOS.length)];
-
-    // just_notify 是給 browser action 用的，強制顯示「空格之神顯靈了」
-    if (just_notify) {
-      alertify.custom(msg, 1500, true);
-    }
-    else {
-      chrome.runtime.sendMessage({purpose: 'can_notify'},
-        function(response) {
-          if (response.result) {
-            alertify.custom(msg, 1500, true);
-          }
-        }
-      );
-    }
-  }
-}
 
 function ask_can_spacing() {
   chrome.runtime.sendMessage({purpose: 'can_spacing'},
