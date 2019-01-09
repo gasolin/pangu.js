f9cbeb3a32403c5d4a50bd44d7f559a41b988236
Vinta <vinta.chen@gmail.com>
2019-01-10 01:39:11 +0800

    WIP

--- browser_extensions/chrome/js/content_script.js
+++ browser_extensions/chrome/js/content_script.js
@@ -2,28 +2,27 @@ var is_spacing = false; // 是不是正在插入空格？
 var already_bind = false; // 是不是已經對 document 綁定過 event？
 var node_hash = {}; // 避免同一個 DOM node 一直觸發 DOMSubtreeModified
 
-function go_page_spacing() {
-  is_spacing = true;
-  pangu.spacingPage();
-  is_spacing = false;
-}
+// function go_page_spacing() {
+//   is_spacing = true;
+//   pangu.spacingPage();
+//   is_spacing = false;
+// }
 
-function go_node_spacing(node) {
-  if (!node.textContent) {
-    return false;
-  }
+// function go_node_spacing(node) {
+//   if (!node.textContent) {
+//     return false;
+//   }
 
-  is_spacing = true;
-  pangu.spacingNode(node);
-  is_spacing = false;
-}
-
+//   is_spacing = true;
+//   pangu.spacingNode(node);
+//   is_spacing = false;
+// }
 
 function ask_can_spacing() {
   chrome.runtime.sendMessage({purpose: 'can_spacing'},
     function(response) {
       if (response.result) {
-        go_page_spacing();
+        // go_page_spacing();
 
         // TODO
         // 改用 callback / promise
@@ -37,26 +36,35 @@ function ask_can_spacing() {
          */
         if (!already_bind) {
           document.body.addEventListener('DOMSubtreeModified', function(e) {
-            if (!is_spacing) {
-              pangu.spacingPageTitle();
+            Han(document.body)
+            .initCond()
+            // .renderElem()
+            // .renderHanging()
+            // .renderJiya()
+            .renderHWS()
+            // .correctBasicBD()
+            // .substCombLigaWithPUA()
 
-              // 一開始拿到的 e.target 似乎不會是最終的 node，所以設個 timer
-              setTimeout(function() {
-                var node = e.target;
-                var id = node.id;
+            // if (!is_spacing) {
+            //   pangu.spacingPageTitle();
 
-                if (id) {
-                  var spacing_timer = node_hash[id];
-                  clearTimeout(spacing_timer);
-                  node_hash[id] = setTimeout(function() {
-                    go_node_spacing(node);
-                  }, 10);
-                }
-                else {
-                  go_node_spacing(node);
-                }
-              }, 100);
-            }
+            //   // 一開始拿到的 e.target 似乎不會是最終的 node，所以設個 timer
+            //   setTimeout(function() {
+            //     var node = e.target;
+            //     var id = node.id;
+
+            //     if (id) {
+            //       var spacing_timer = node_hash[id];
+            //       clearTimeout(spacing_timer);
+            //       node_hash[id] = setTimeout(function() {
+            //         go_node_spacing(node);
+            //       }, 10);
+            //     }
+            //     else {
+            //       go_node_spacing(node);
+            //     }
+            //   }, 100);
+            // }
           }, false);
 
           already_bind = true;
